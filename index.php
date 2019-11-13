344f0e83b063b095b3719cc1469f8d981b79c439
Franck17000 <55445120+Franck17000@users.noreply.github.com>
2019-11-13 16:38:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,3 @@
 </div>
 </html>
 
-
-<?php
-require_once('src/Router.php');
-
-$request = new Src\Request();
-$router = new Src\Router($request);
-
-require_once('app/routes.php');
-
-echo $router->render();
-?>
